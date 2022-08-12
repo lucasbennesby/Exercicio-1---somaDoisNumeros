597ef3cd16c0dd2a34308d3b46add329c698a7ce
lucasbennesby <lucasbenesby@gmail.com>
2022-08-12 20:50:51 -0300

    Atividade soma dois numeros com java

---
+++ somaDoisNumeros.java
@@ -0,0 +1,11 @@
+public class somaDoisNumeros {
+    public static void main(String[] args) {
+        int valor1 = 8;
+        int valor2 = 4;
+        int soma;
+
+        soma = valor1 + valor2;
+        System.out.print(soma);
+
+    }
+}
---
+++ somaDoisNumeros.js
@@ -0,0 +1,7 @@
+let num = [5, 7];
+let soma = 0;
+
+for (i = 0; i < num.length; i++) {
+  soma += num[i];
+}
+console.log(soma);
